5c02fcee5c3211f602b431a3c918cc6f6538e2e1
Martín Venegas M <64295294+Martin-Venegas-M@users.noreply.github.com>
2024-07-29 16:00:41 -0400

    Initial commit

---
+++ processing/trabajo_final/multivariados.R
@@ -0,0 +1,222 @@
+
+###########################################################################
+# 0. Identificación -------------------------------------------------------
+#Título: Código de multivariados de EPSOC 2018
+#Institución: PUC
+#Encargado: Martín Venegas - Estudiante
+
+# Resumen ejecutivo: El presente documento contiene el código para calcular los
+# los multivariados de EPSOC 2018 procesada
+# El objetivo es poder contar con tablas y gráficos para la realización
+# del trabajo final
+# El producto de este script serán distintas tablas y gráficos de multivariados
+###########################################################################
+
+# 1. Cargar librerías -----------------------------------------------------
+
+if (!require("pacman")) install.packages("pacman")  #si falta pacman, instalar
+
+pacman::p_load(tidyverse,
+               sjmisc,
+               sjPlot,
+               summarytools,
+               sjlabelled,
+               car,
+               haven,
+               psych,
+               lavaan,
+               semTable,
+               semPlot
+               )
+
+# 2. Cargar datos ---------------------------------------------------------
+
+epsoc <- readRDS("input/data/proc/epsoc_proc.RDS")
+
+
+# 3. EFA ------------------------------------------------------------------
+
+bsjo <- epsoc %>% rename(
+  IGUAL1 = bsjo1,
+  IGUAL2 = bsjo2,
+  IGUAL3 = bsjo3,
+  NEC1 = bsjo4,
+  NEC2 = bsjo5,
+  NEC3 = bsjo6,
+  EQUI1 = bsjo7,
+  EQUI2 = bsjo8,
+  EQUI3 = bsjo9,
+  EQUI4 = bsjo13,
+  EQUI5 = bsjo14,
+  DER1 = bsjo10,
+  DER2 = bsjo11,
+  DER3 = bsjo12
+  )
+
+sjPlot::tab_corr(bsjo, triangle = "lower")
+
+# KMO
+KMO(bsjo)
+
+# Prueba de esfericidad de Bartlett
+cortest.bartlett(bsjo)
+
+# Gráfico de sedimentación con autovalores
+plot(scree(bsjo))
+
+# Análisis paralelo
+set.seed(06051997) # Resultado reproducible
+fa.parallel(bsjo, 
+            fm = 'ml', # método máxima verosimilitud
+            fa = 'fa') # tipo de autovalores: análisis factorial
+
+# Ver comunalidades en solución de 4 factores
+
+fa(bsjo, nfactors = 4, 
+   fm = "ml", 
+   rotate = "varimax") # rotación varimax
+
+fa(bsjo, nfactors = 3, 
+   fm = "ml", 
+   rotate = "varimax") # rotación varimax
+
+fa(bsjo, nfactors = 2, 
+   fm = "ml", 
+   rotate = "varimax") # rotación varimax
+
+# 4. CFA ------------------------------------------------------------------
+
+# Definir modelo de medición
+
+mod1 <- '
+igualdad =~  IGUAL1 + IGUAL2 + IGUAL3
+necesidad =~ NEC1 + NEC2 + NEC3
+equidad =~   EQUI1 + EQUI2 + EQUI3
+derecho =~   DER1 + DER2 + DER3
+'
+
+mod2 <- '
+igualdad =~  IGUAL1 + IGUAL2 
+necesidad =~ NEC1 + NEC2
+equidad =~   EQUI1 + EQUI2
+derecho =~   DER1 + DER2
+'
+
+mod3 <- '
+igualdad =~  IGUAL1 + IGUAL2 + IGUAL3
+necesidad =~ NEC1 + NEC2 + NEC3
+equidad =~   EQUI1 + EQUI2 + EQUI3 + EQUI4 + EQUI5
+derecho =~   DER1 + DER2 + DER3
+'
+
+#bsjo <- bsjo %>% add_column(epsoc_proc %>% select(sexo_rec, hijos_rec, clase_perc_rec))
+
+# Ajustar modelo CFA
+mod1_cfa <- cfa(mod1, data = bsjo)
+mod2_cfa <- cfa(mod2, data = bsjo)
+mod3_cfa <- cfa(mod3, data = bsjo)
+
+
+# Resultados
+## Salida general
+# summary(mod1_cfa,
+#         standardized = TRUE, # mostrar cargas estandarizadas
+#         fit.measures = TRUE) # mostrar índices de ajuste extendidos
+# 
+# summary(mod2_cfa,
+#         standardized = TRUE, # mostrar cargas estandarizadas
+#         fit.measures = TRUE) # mostrar índices de ajuste extendidos
+# 
+# summary(mod3_cfa,
+#         standardized = TRUE, # mostrar cargas estandarizadas
+#         fit.measures = TRUE) # mostrar índices de ajuste extendidos
+
+# Ver solo índices de ajuste
+fitmeasures(mod1_cfa, fit.measures = c("chisq", "df", "pvalue", "cfi", "rmsea"))
+fitmeasures(mod2_cfa, fit.measures = c("chisq", "df", "pvalue", "cfi", "rmsea"))
+fitmeasures(mod3_cfa, fit.measures = c("chisq", "df", "pvalue", "cfi", "rmsea"))
+
+semPaths(mod1_cfa, 
+         whatLabels="std", 
+         layout="tree2", 
+         title = FALSE, # Elimina el título
+         rotation = 4,
+         sizeMan=5, sizeLat=15, # Ajusta el tamaño de los nodos
+         edge.label.cex = 1, # Ajusta el tamaño del texto de las etiquetas de las aristas
+         nCharNodes = 0, # Muestra todos los caracteres de los nombres de los nodos
+         curvature = 0.5, # Ajusta la curvatura de las aristas
+         residuals = FALSE, # Muestra las varianzas de los errore
+         exoCov = F,
+         shapeLat = "ellipse",
+         shapeMan = "rectangle"
+)
+
+semPaths(mod2_cfa, 
+         whatLabels="std", 
+         layout="tree2", 
+         title = FALSE, # Elimina el título
+         rotation = 4,
+         sizeMan=5, sizeLat=15, # Ajusta el tamaño de los nodos
+         edge.label.cex = 1, # Ajusta el tamaño del texto de las etiquetas de las aristas
+         nCharNodes = 0, # Muestra todos los caracteres de los nombres de los nodos
+         curvature = 0.5, # Ajusta la curvatura de las aristas
+         residuals = FALSE, # Muestra las varianzas de los errore
+         exoCov = F,
+         shapeLat = "ellipse",
+         shapeMan = "rectangle"
+)
+
+semPaths(mod3_cfa, 
+         whatLabels="std", 
+         layout="tree2", 
+         title = FALSE, # Elimina el título
+         rotation = 4,
+         sizeMan=5, sizeLat=15, # Ajusta el tamaño de los nodos
+         edge.label.cex = 1, # Ajusta el tamaño del texto de las etiquetas de las aristas
+         nCharNodes = 0, # Muestra todos los caracteres de los nombres de los nodos
+         curvature = 0.5, # Ajusta la curvatura de las aristas
+         residuals = FALSE, # Muestra las varianzas de los errore
+         exoCov = F,
+         shapeLat = "ellipse",
+         shapeMan = "rectangle"
+)
+
+# 5. SEM ------------------------------------------------------------------
+
+
+
+bsjo <- cbind(bsjo, dummy.code(bsjo$educ_rec))
+bsjo <- cbind(bsjo, dummy.code(bsjo$pueblo_rec))
+bsjo <- cbind(bsjo, dummy.code(bsjo$actividad_principal_rec))
+bsjo <- cbind(bsjo, dummy.code(bsjo$hijos_rec))
+bsjo <- cbind(bsjo, dummy.code(bsjo$clase_perc_rec))
+
+bsjo <- janitor::clean_names(bsjo)
+
+## Especificar el modelo: medición y estructural
+m_sem1 <- '
+# Modelo medición
+igualdad =~  igual1 + igual2
+necesidad =~ nec1 + nec2
+equidad =~   equi1 + equi2
+derecho =~   der1 + der2
+
+  # Modelo estructural
+igualdad ~  sexo_rec + universitaria_y_posgrado + tecnica_superior + nsnr + pertenece + nsnr_2 + trabajo_en_la_ocupacion + nsnr_3 + uno_o_dos + tres_o_mas + nsnr_4 + clase_media_baja + clase_media_alta_o_alta + nsnr_5
+necesidad ~ sexo_rec + universitaria_y_posgrado + tecnica_superior + nsnr + pertenece + nsnr_2 + trabajo_en_la_ocupacion + nsnr_3 + uno_o_dos + tres_o_mas + nsnr_4 + clase_media_baja + clase_media_alta_o_alta + nsnr_5
+equidad ~   sexo_rec + universitaria_y_posgrado + tecnica_superior + nsnr + pertenece + nsnr_2 + trabajo_en_la_ocupacion + nsnr_3 + uno_o_dos + tres_o_mas + nsnr_4 + clase_media_baja + clase_media_alta_o_alta + nsnr_5
+derecho ~   sexo_rec + universitaria_y_posgrado + tecnica_superior + nsnr + pertenece + nsnr_2 + trabajo_en_la_ocupacion + nsnr_3 + uno_o_dos + tres_o_mas + nsnr_4 + clase_media_baja + clase_media_alta_o_alta + nsnr_5
+'
+
+## Ajustar modelo
+f_sem1 <- sem(m_sem1, data = bsjo)
+
+## Ver resultados completos
+summary(f_sem1, fit.measures = T, standardized = T, 
+        rsquare = T, modindices = T)
+
+## Exportar tablas
+### Ajustar versión estandarizada
+semTable(f_sem1, type = "html", 
+         paramSets = c("loadings", "slopes", "fits", "latentcovariances"),
+         file = "resultados_actividad7")
